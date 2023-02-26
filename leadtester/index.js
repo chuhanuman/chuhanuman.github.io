1657033670d7b5bfde44956843b3a4fed94a021e
Hanuman Chu <chuhanuman@yahoo.com>
2023-02-25 22:28:19 -0700

    Changed image to canvas

--- leadtester/index.js
+++ leadtester/index.js
@@ -1,6 +1,11 @@
 function loadImage(input) {
 	if (input.files && input.files[0]) {
 		var waterImage = document.getElementById('waterImage');
-		waterImage.src = URL.createObjectURL(input.files[0]);
+		var ctx = waterImage.getContext("2d");
+		var tempImage = new Image;
+		tempImage.onload = function(){
+		  ctx.drawImage(tempImage,0,0);
+		};
+		tempImage.src = URL.createObjectURL(input.files[0]);
 	}
 }
